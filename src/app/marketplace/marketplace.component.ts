dae20f0f3c08dc28bbb773862d46870ad92a3ee7
Justin Roller <justin@>
2018-06-11 10:55:11 -0700

    added bootstrap and clickable album detail.

--- src/app/marketplace/marketplace.component.ts
+++ src/app/marketplace/marketplace.component.ts
@@ -1,19 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Album } from '../album.model';
 import { Router } from '@angular/router';
+import { AlbumService } from '../album.service';
 
 @Component({
   selector: 'app-marketplace',
   templateUrl: './marketplace.component.html',
-  styleUrls: ['./marketplace.component.css']
+  styleUrls: ['./marketplace.component.css'],
+  providers: [AlbumService]
 })
 
 
-export class MarketplaceComponent {
-  constructor(private router: Router){}
+export class MarketplaceComponent implements OnInit {
+  albums: Album[];
 
-  
- goToDetailPage(clickedAlbum: Album) {
-     this.router.navigate(['albums', clickedAlbum.id]);
-   };
+  constructor(private router: Router, private albumService: AlbumService){}
+
+  ngOnInit(){
+    this.albums = this.albumService.getAlbums();
+  }
+
+  goToDetailPage(clickedAlbum: Album) {
+    this.router.navigate(['albums', clickedAlbum.id]);
+  };
 }
